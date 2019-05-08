d26f8cf86a07f3a8f0ddd871629c35e7ed6995a3
yura_koala <yura.l@koalamasters.com>
2019-05-08 17:20:45 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors',1);
+header('Content-Type: text/html; charset=utf-8;');
+?>
+
+<html>
+<head>
+    <title>test</title>
+    <script src="/vendor/public/bootstrap/dist/js/bootstrap.js"></script>
+    <link type="text/css" href="/style/style.less">
+</head>
+
+<body>
+
+<h1>test</h1>
+<div class="color-red">
+    Lorem ipsum
+</div>
+
+</body>
+</html>
